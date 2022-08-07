3f9be641b16d9e0360aeee50c8d8ce9a993d2442
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-08-07 19:05:50 +0100

    display images

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+//variables
+const gallery = document.getElementById("gallery");
+const popup = document.getElementById("popup");
+const selectedImage = document.getElementById("selectedImage");
+const imageIndexes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const selectedIndex = null;
+
+imageIndexes.forEach((index) => {
+  const image = document.createElement("img");
+  image.src = `/images/${index}.jpg`;
+  image.alt = `Sport photo ${index}`;
+  image.classList.add("galleryImg");
+
+  image.addEventListener("click", () => {
+    //popup stuuf
+  });
+
+  gallery.appendChild(image);
+});
