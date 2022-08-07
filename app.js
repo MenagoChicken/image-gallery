908ffe029e84e83ab32ecb2b97213882862f06dc
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-08-07 19:39:28 +0100

    image gallery finished
    next step optimising image sizes

--- app.js
+++ app.js
@@ -6,14 +6,26 @@ const imageIndexes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const selectedIndex = null;
 
 imageIndexes.forEach((index) => {
+  const immageDiv = document.createElement("div");
+  immageDiv.classList.add("imageContainer");
+
   const image = document.createElement("img");
   image.src = `/images/${index}.jpg`;
   image.alt = `Sport photo ${index}`;
   image.classList.add("galleryImg");
 
   image.addEventListener("click", () => {
-    //popup stuuf
+    //popup stuff
+    popup.style.transform = "translateY(0)";
+    selectedImage.src = `/images/${index}.jpg`;
   });
 
-  gallery.appendChild(image);
+  immageDiv.appendChild(image);
+  gallery.appendChild(immageDiv);
+});
+
+popup.addEventListener("click", () => {
+  popup.style.transform = "translate(-100%)";
+  popup.src = "";
+  popup.alt = "";
 });
